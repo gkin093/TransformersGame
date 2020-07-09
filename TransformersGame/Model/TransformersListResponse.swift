8476e592c4d7d9ceedae4b913b6dfe03c49a6cae
Kin <kin_t_uno@yahoo.com.br>
2020-07-09 02:09:34 -0300

    create all API calls

---
+++ TransformersGame/Model/TransformersListResponse.swift
@@ -0,0 +1,13 @@
+//
+//  TransformersListResponse.swift
+//  TransformersGame
+//
+//  Created by Gustavo Kin on 09/07/20.
+//  Copyright © 2020 Gustavo Kin. All rights reserved.
+//
+
+import Foundation
+
+class TransformersListResponse: Codable {
+    let transformers: [Transformer]
+}
--- TransformersGame/ViewController.swift
+++ TransformersGame/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let viewModel = ViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        viewModel.generateToken()
     }
 
-
+    @IBAction func listar(_ sender: UIButton) {
+        viewModel.listTransformers()
+    }
+    
+    @IBAction func criar(_ sender: Any) {
+        let transformer = Transformer(name: "Teste", strength: 2, intelligence: 4, speed: 4, endurance: 5, rank: 6, courage: 5, firepower: 6, skill: 6, team: "D")
+        viewModel.create(transformer)
+        
+    }
+    
+    @IBAction func atualizar(_ sender: Any) {
+        let transformer = Transformer(name: "Teste", strength: 2, intelligence: 4, speed: 4, endurance: 5, rank: 6, courage: 5, firepower: 6, skill: 6, team: "D")
+        viewModel.update(transformer)
+    }
+    
+    @IBAction func deletar(_ sender: Any) {
+        viewModel.delete(id: "dfsadf")
+    }
 }
 
